b2c2a3dc0e6d69310fa5c4641ae2cc630a500cd7
kyanro <gw.fnct+github-pub@gmail.com>
2013-02-07 00:55:51 +0900

    右クリックから実行で引数を受け付ける感じに変更。out-GridView出力にも変更。
    文字コードをutf8に。

--- 1週間以内に更新されたファイルを表示/Get-RecentlyFile.ps1
+++ 1週間以内に更新されたファイルを表示/Get-RecentlyFile.ps1
@@ -5,6 +5,24 @@ param(
     $date=(Get-Date).AddDays(-7)
 )
 
+$inputPattern = Read-Host "検索したいパターン（正規表現）を入力するといいよ"
 
-Get-ChildItem | ? LastWriteTime -ge $date | ? Name -Match $pattern | sort Name | Format-Table Name,LastWriteTime 
+if(($inputPattern -eq $null) -or ($inputPattern -eq "")){
+    $pattern = $inputPattern
+}
+
+$inputFromDate = Read-Host "検索の起点にしたい日付を入力するといいよ(yyyy/m/d)"
+if(($inputFromDate -eq $null) -or ($inputFromDate -eq "")){
+    #入力がなければデフォルトのままで。
+    Write-Host ("デフォルトの日付を使うよ {0}" -f $date.ToString("yyyy/M/d"))
+}else{
+    if($inputFromDate -as [datetime]){
+        $date = $inputFromDate
+    }else{
+        Write-Host ("日付データが不正だからデフォルトの日付を使うよ {0}" -f $date.ToString("yyyy/M/d"))
+    }
+}
+
+
+Get-ChildItem | ? LastWriteTime -ge $date | ? Name -Match $pattern | sort Name | Out-GridView
 
